0e9dc97e444ad49d3349a8ddf3e659a6317cbae3
Ed Nutting <git@flingos.co.uk>
2017-02-27 22:10:29 +0000

    Totally new way of doing it?

--- LinuxDeviceService/RestfulClient.py
+++ LinuxDeviceService/RestfulClient.py
@@ -47,10 +47,11 @@ print interval
 payload = None
 if (mode == "rpi"):
     print "Using 'rpi' mode"
-    tempStr = check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
-    temp = tempStr.split("=")[1].split("'")[0]
-    print payload
-    payload = '{ "data": [{ "hardware_id": "' + id + '", "sensor_info": [{ "tag": "cpu_temperature", "value": ' + temp + ' }] }] }'
+    #tempStr = check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
+    #temp = tempStr.split("=")[1].split("'")[0]
+    temp = 0.5 #float(temp)
+    print temp
+    payload = { "data": [{ "hardware_id": id, "sensor_info": [{ "tag": "cpu_temperature", "value": temp }] }] }
     print payload
     print "Uploading reading"
     POST("reading/" + id, payload)
