2d731a4c025a7fe8f95b44801c4230b11b4878c9
Ed Nutting <git@flingos.co.uk>
2017-02-28 05:59:44 +0000

    Add PowerShell install script for Windows setup

--- WindowsDeviceService/HeatMapMonitor_Windows/PowerShellInstall/InstallHeatMap.ps1
+++ WindowsDeviceService/HeatMapMonitor_Windows/PowerShellInstall/InstallHeatMap.ps1
@@ -1,5 +1,36 @@
 ﻿#
 # InstallHeatMap.ps1
 #
+#Requires -RunAsAdministrator
 
-wget https://github.com/jgbdev/HeatMap/raw/master/Releases/HeatMap_ServiceLatest.zip -OutFile HeatMap
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+if (Test-Path .\HeatMapService.zip) {
+	Remove-Item .\HeatMapService.zip -Force
+}
+if (Test-Path .\HeatMapService) {
+	Remove-Item .\HeatMapService -Force -Recurse
+}
+
+wget https://github.com/jgbdev/HeatMap/raw/master/Releases/HeatMap_ServiceLatest.zip -OutFile .\HeatMapService.zip
+
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+Unzip ((Get-Item -Path ".\" -Verbose).FullName + "\HeatMapService.zip") ((Get-Item -Path ".\" -Verbose).FullName + "\HeatMapService")
+
+If(Test-Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319") {
+	& "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" -u ".\HeatMapService\HeatMap_Service.exe"
+	& "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" ".\HeatMapService\HeatMap_Service.exe"
+}
+ElseIf(Test-Path "C:\Windows\Microsoft.NET\Framework\v4.0.30319") {
+	& "C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" -u ".\HeatMapService\HeatMap_Service.exe"
+	& "C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" ".\HeatMapService\HeatMap_Service.exe"
+}
+Else {
+	"InstallUtil not found! Please use the manual install instructions."
+}
